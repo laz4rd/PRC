587575f36393cefcf98beb115c93857cd1d96f4b
laz4rd <brijrajbhati123@gmail.com>
2025-11-04 16:08:08 +0530

    modified:   Question5.java

--- Question5.java
+++ Question5.java
@@ -2,7 +2,32 @@
 
 import java.util.*;
 
+class Rectangle //Rectangle Class
+{
+    double length;
+    double width;
+
+    Rectangle(double length, double width) {
+
+        this.length=length;
+        this.width=width;
+    }
+    
+    double Area()
+    {
+        double area=this.length*this.width;
+        return area;
+    }
+}
 public class Question5
 {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        double Length=sc.nextDouble();
+        double Width=sc.nextDouble();
+
+        Rectangle rect=new Rectangle(Length, Width);
+        System.out.println("The Area of the Rectangle is "+rect.Area()+" sq.units");
+    }
 }
