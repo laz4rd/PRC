1d7582aa7cb831623681d1d28779f600a3838079
laz4rd <brijrajbhati123@gmail.com>
2025-11-04 18:56:05 +0530

    new file:   Question14.java 	new file:   Question15.java 	modified:   example.txt

---
+++ Question14.java
@@ -0,0 +1,20 @@
+// Write a Java program to create a text file named example.txt and 
+// write "Hello Java File Handling!" inside it.
+
+import java.io.*;
+
+public class Question14
+{
+    public static void main(String[] args) {
+        try {
+            FileWriter fw = new FileWriter("example.txt");
+
+            fw.write("Welcome to File Handling");
+            fw.close();
+            System.out.println("Completed");
+
+        } catch (IOException e) {
+            System.out.println("Had an Error trying");
+        }
+    }
+}
