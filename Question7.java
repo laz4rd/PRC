a0245d625becb58d03478ab016d86dc3dccaf3dd
laz4rd <brijrajbhati123@gmail.com>
2025-11-04 17:07:41 +0530

    modified:   Question7.java 	new file:   com/laz4rd/mypackages/Calculator.java

---
+++ Question7.java
@@ -0,0 +1,14 @@
+// File: MainProgram.java
+import com.laz4rd.mypackages.Calculator;
+
+public class Question7 {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        
+        int sum = calc.add(10, 5);
+        int difference = calc.subtract(10, 5);
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+    }
+}
---
+++ com/laz4rd/mypackages/Calculator.java
@@ -0,0 +1,18 @@
+// Create a package mypack containing a class Calculator with
+// methods add(int, int) and subtract(int, int).
+// Import and use this package in another program.
+
+// File: mypack/Calculator.java
+package com.laz4rd.mypackages;
+
+public class Calculator {
+    // Method to add two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method to subtract two numbers
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
